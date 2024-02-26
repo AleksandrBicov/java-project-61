1578d7cb1fae5591ebb5e4f39ef96161975d007f
drakylo <erldimitrovgrad@gmail.com>
2024-02-26 16:57:54 +0300

    Add Prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -1,9 +1,58 @@
 package hexlet.code.games;
 
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+
+import java.util.Scanner;
+
 public final class Prime {
     private Prime() {
     }
     public static void prime() {
-        System.out.println("hello");
+        Cli.askName();
+        String name = Cli.getName();
+        final int minValue = 1;
+        final int maxValue = 500;
+        final int repeat = 3;
+        for (int i = 0; i < repeat; i++) {
+            int randomValue =
+                minValue + (int) (Math.random() * (maxValue - minValue + 1));
+            String question =
+               "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
+            String expression = "" + randomValue;
+
+            Engine.hello(question, expression);
+
+            Scanner scan = new Scanner(System.in);
+            String input = scan.nextLine();
+
+            input = input.toLowerCase();
+            String answer;
+
+            if (isSimple(randomValue)) {
+                answer = "yes";
+            } else {
+                answer = "no";
+            }
+
+            if (!input.equals(answer)) {
+                Engine.loose(input, answer, name);
+                return;
+            }
+
+            System.out.println("Correct!");
+        }
+        System.out.println("Congratulations," + name + "!");
+    }
+    public static boolean isSimple(final int number) {
+        if (number < 2) {
+            return false;
+        }
+        for (int i = 2; i < number / 2; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
