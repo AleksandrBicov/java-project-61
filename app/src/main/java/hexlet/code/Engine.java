3c26f6deefdb99a5029856a09e9211d98fe4e39d
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-02-29 19:45:11 +0300

    change Calc.java and Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,7 +1,32 @@
 package hexlet.code;
 
+import hexlet.code.games.Calc;
+
+import java.util.Scanner;
+
 public final class Engine {
     private Engine() {
+    }
+    public static int engine(String question,String expression,int answer,int index) {
+        final int repeat = 2;
+        String name = Cli.getName();
+        System.out.println(question);
+        System.out.println("question : " + expression);
+        System.out.println("Your answer: ");
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        String inputToString = "" + input;
+        String answerToString = "" + answer;
+        boolean result = input != answer;
+        if (result) {
+            Engine.loose(inputToString, answerToString, name);
+            return input;
+        }
+        System.out.println("Correct!");
+        if (index == repeat) {
+        System.out.println("Congratulations," + name + "!");
+        }
+        return input;
     }
     public static void hello(final String question, final String expression) {
         System.out.println(question);
--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -2,6 +2,7 @@ package hexlet.code.games;
 
 import hexlet.code.Cli;
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 import java.util.Scanner;
 
@@ -10,46 +11,37 @@ public final class  Calc {
     }
     public static void calc() {
         Cli.askName();
-        String name = Cli.getName();
-        final int minValue = 1;
-        final int maxValue = 10;
-        final int maxOperator = 3;
         final int repeat = 3;
-        for (int i = 0; i < repeat; i++) {
-        int randomValue =
-            minValue + (int) (Math.random() * (maxValue - minValue + 1));
-        int randomValue1 =
-            minValue + (int) (Math.random() * (maxValue - minValue + 1));
-        int randomOperator =
-            minValue + (int) (Math.random() * (maxOperator - minValue + 1));
-        char operator = " +-*".charAt(randomOperator);
+        for (int index = 0; index < repeat; index++) {
+            int randomValue = Utils.generateNumber(1, 10);
+            int randomValue1 = Utils.generateNumber(1, 10);
+            int randomOperator = Utils.generateNumber(1, 3);
+            char operator = " +-*".charAt(randomOperator);
             String question = "What is the result of the expression?";
             String expression = "" + randomValue + operator + randomValue1;
-            Engine.hello(question, expression);
-        Scanner scan = new Scanner(System.in);
-        int input = scan.nextInt();
-        int answer = 0;
-            switch (operator) {
-                case ('+'):
-                    answer = randomValue + randomValue1;
-                    break;
-                case ('-'):
-                    answer = randomValue - randomValue1;
-                    break;
-                case ('*'):
-                    answer = randomValue * randomValue1;
-                    break;
-                default:
-                    break;
+            int answer = Calc.gameCalc(randomValue, randomValue1, operator);
+            int input = Engine.engine(question, expression, answer,index);
+            if (input != answer) {
+                return;
             }
-            String inputToString = "" + input;
-            String answerToString = "" + answer;
-        if (input != answer) {
-            Engine.loose(inputToString, answerToString, name);
-            return;
+
         }
-        System.out.println("Correct!");
+    }
+    public static int gameCalc(int randomValue,int randomValue1,char operator ){
+        int answer = 0;
+        switch (operator) {
+            case ('+'):
+                answer = randomValue + randomValue1;
+                break;
+            case ('-'):
+                answer = randomValue - randomValue1;
+                break;
+            case ('*'):
+                answer = randomValue * randomValue1;
+                break;
+            default:
+                break;
         }
-        System.out.println("Congratulations," + name + "!");
+        return answer;
     }
 }
