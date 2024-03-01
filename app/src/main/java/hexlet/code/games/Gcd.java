a55663f9ca280bab68a53752a8a87901a0c50b76
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-01 16:52:41 +0300

    change Gcd

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -2,6 +2,7 @@ package hexlet.code.games;
 
 import hexlet.code.Cli;
 import hexlet.code.Engine;
+import hexlet.code.Utils;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -12,60 +13,27 @@ public final class Gcd {
     public static void gcd() {
         Cli.askName();
         String name = Cli.getName();
-        final int minValue = 1;
-        final int maxValue = 100;
         final int repeat = 3;
-        for (int i = 0; i < repeat; i++) {
-            int randomValue =
-                minValue + (int) (Math.random() * (maxValue - minValue + 1));
-            int randomValue1 =
-                minValue + (int) (Math.random() * (maxValue - minValue + 1));
+        for (int index = 0; index < repeat; index++) {
+            int randomValue = Utils.generateNumber(1, 100);
+            int randomValue1 = Utils.generateNumber(1, 100);
             String question =
                 "Find the greatest common divisor of given numbers.";
             String expression =  randomValue + " " + randomValue1;
-            Engine.hello(question, expression);
-            Scanner scan = new Scanner(System.in);
-            int input = scan.nextInt();
-            while (randomValue != 0) {
-                int tmp = randomValue1 % randomValue;
-                randomValue1 = randomValue;
-                randomValue = tmp;
-            }
-            String inputToString = "" + input;
-            String answerToString = "" + randomValue1;
-            if (input != randomValue1) {
-                Engine.loose(inputToString, answerToString, name);
+            int answer = gameGcd(randomValue, randomValue1);
+            int input = Engine.engine(question, expression, answer,index);
+            if (input != answer) {
                 return;
             }
-            System.out.println("Correct!");
         }
-        System.out.println("Congratulations," + name + "!");
     }
-
-    public static class Progression {
-        public static void progression() {
-            final int minValue = 1;
-            final int maxValue = 9;
-            final int newArr = 10;
-            int[] array = new int[newArr];
-            int randomValue =
-               minValue + (int) (Math.random() * (maxValue - minValue + 1));
-            int randomValue1 =
-               minValue + (int) (Math.random() * (maxValue - minValue + 1));
-            int randomValue2 =
-               minValue + (int) (Math.random() * (maxValue - minValue + 1));
-            array[0] = randomValue1;
-            for (int i = 1; i < newArr; i++) {
-                array[i] = randomValue + array[i - 1];
-            }
-            array[randomValue2] = 0;
-            String arr = Arrays.toString(array);
-            String arr2 = arr.substring(1, arr.length() - 1);
-            String arr3 = arr2.replace(",", "");
-            String arr4 = arr3.replace(" 0", " ..");
-            System.out.println(arr4);
-
+    public static int gameGcd(int randomValue, int randomValue1){
+        while (randomValue != 0) {
+            int tmp = randomValue1 % randomValue;
+            randomValue1 = randomValue;
+            randomValue = tmp;
         }
+        return randomValue1;
     }
 }
 
