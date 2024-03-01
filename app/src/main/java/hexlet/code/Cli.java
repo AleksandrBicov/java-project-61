7f24d0b90d141c6e63775039ab8426dc8b9976fb
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-01 18:22:27 +0300

    change Cli.java

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -10,5 +10,13 @@ public final class Cli {
         System.out.println("May I have your name?");
         name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
+
+        // scanner.close();
+        // Если добавляю эту строку весь проект падает с ошибкой
+        // Execution failed for task ':App.main()'.
+        //> Process 'command '/usr/lib/jvm/jdk-20/bin/java'' finished with non-zero exit value 1
+        // не могу понять что с этим делать? или вообще с этим файлом не взаимодействовать ,
+        //а для приветствия и работы с именем повотрно код писать?
+
     }
 }
