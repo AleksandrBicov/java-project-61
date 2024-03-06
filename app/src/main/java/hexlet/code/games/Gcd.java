3ffabcdeb4d2a57b206716b79855e84d9758001e
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-06 18:42:39 +0300

    change Gcd.java

--- app/src/main/java/hexlet/code/games/Gcd.java
+++ app/src/main/java/hexlet/code/games/Gcd.java
@@ -1,6 +1,5 @@
 package hexlet.code.games;
 
-import hexlet.code.Cli;
 import hexlet.code.Engine;
 import hexlet.code.Utils;
 
@@ -9,25 +8,28 @@ public final class Gcd {
     private Gcd() {
     }
     public static void gcd() {
-        Cli.askName();
-        final int repeat = 3;
-        for (int index = 0; index < repeat; index++) {
-            int randomValue = Utils.generateNumber(1, 100);
-            int randomValue1 = Utils.generateNumber(1, 100);
-            String question =
+        Engine.askName();
+        String question =
                 "Find the greatest common divisor of given numbers.";
-            String expression =  randomValue + " " + randomValue1;
-            int answer = gameGcd(randomValue, randomValue1);
-          //  int input = Engine.engine(question, expression, answer,index);
+        final int repeat = 3;
+        final int finalGenerate = 100;
+        String[][] game = new String[repeat][2];
+        for (int index = 0; index < repeat; index++) {
+            int randomValue = Utils.generateNumber(1, finalGenerate);
+            int randomValue1 = Utils.generateNumber(1, finalGenerate);
+            game[index][0] = randomValue + " " + randomValue1;
+            game[index][1] = gameGcd(randomValue, randomValue1);
         }
+            Engine.engine(question, game);
     }
-    public static int gameGcd(int randomValue, int randomValue1){
+    public static String gameGcd(int randomValue, int randomValue1) {
         while (randomValue != 0) {
             int tmp = randomValue1 % randomValue;
             randomValue1 = randomValue;
             randomValue = tmp;
         }
-        return randomValue1;
+        String answer = "" + randomValue1;
+        return answer;
     }
 }
 
