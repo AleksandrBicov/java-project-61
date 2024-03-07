ae3ff5895666c3b81c0df146641d3ecf42ff223c
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-07 20:16:43 +0300

    change Progression.java

--- app/src/main/java/hexlet/code/games/Progression.java
+++ app/src/main/java/hexlet/code/games/Progression.java
@@ -4,29 +4,30 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 import java.util.Arrays;
 final public class Progression {
+
     private Progression() {
     }
     public static void progression() {
-        final int newArr = 10;
+        final int PROGRESSION_LENGTH = 10;
         String question = "What number is missing in the progression?";
         String[][] game = new String[Engine.ROUNDS][2];
         for (int index = 0; index < Engine.ROUNDS; index++) {
-            int[] array = new int[newArr];
-            int randomValue = Utils.generateNumber(1, 10);
-            int randomValue1 = Utils.generateNumber(1, 10);
-            int randomValue2 = Utils.generateNumber(1, 10);
-            array[0] = randomValue1;
-            for (int i = 1; i < newArr; i++) {
-                array[i] = randomValue + array[i - 1];
-            }
-            array[randomValue2] = 0;
-            String arr = Arrays.toString(array);
-            String arr2 = arr.substring(1, arr.length() - 1);
-            String arr3 = arr2.replace(",", "");
-            game[index][0] = arr3.replace(" 0", " ..");
-            game[index][1] = randomValue * randomValue2 + randomValue1 + "";
+            int hiddenMemberIndex = Utils.generateNumber(1, 10);
+            int first = Utils.generateNumber(1, 10);
+            int step = Utils.generateNumber(1, 10);
+            String[] progression = makeProgression(first, step, PROGRESSION_LENGTH);
+            progression[hiddenMemberIndex] = "..";
+            game[index][0] = String.join(" ", progression);
+            game[index][1] = hiddenMemberIndex * step + first + "";
         }
 
         Engine.engine(question, game);
+    }
+    private static String[] makeProgression(int first, int step, int length) {
+        String[] progression = new String[length];
+        for (int i = 0; i < length; i += 1) {
+            progression[i] = Integer.toString(first + i * step);
+        }
+        return progression;
     }
 }
