3224a12b03c0f8d5b2444d1bc5f9a48b3d654212
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-07 19:31:12 +0300

    change Engine.java

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -7,41 +7,34 @@ public final class Engine {
     }
     public static final int ROUNDS = 3;
     public static void engine(final String question, final String game[][]) {
-        Engine.askName();
-        String name = Engine.name;
-        final int repeat = 3;
-        for (int index = 0; index < repeat; index++) {
-        hello(question, game[index][0]);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        for (int index = 0; index < ROUNDS; index++) {
+        System.out.println(question);
+        System.out.println("question : " + game[index][0]);
+        System.out.println("Your answer: ");
         Scanner scan = new Scanner(System.in);
         String inputString = scan.nextLine();
         inputString = inputString.toLowerCase();
         if (!inputString.equals(game[index][1]))  {
-            Engine.loose(inputString, game[index][1], name);
+            System.out.println("'" + inputString + "' is wrong answer ;"
+                    + "(. Correct answer was" + " '" + game[index][1] + " '.\n"
+                    + "Let's try again," + name + "!");
             return;
         }
         System.out.println("Correct!");
        }
         System.out.println("Congratulations," + name + "!");
     }
-    private static String name;
     public static void askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        name = scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
-
-    }
-    public static void hello(final String question, final String expression) {
-        System.out.println(question);
-        System.out.println("question : " + expression);
-        System.out.println("Your answer: ");
-
-    }
-    public static void loose(final String even, final String answer, final String name) {
-        System.out.println("'" + even + "' is wrong answer ;"
-                + "(. Correct answer was" + " '" + answer + " '.\n"
-                + "Let's try again," + name + "!");
     }
 
 }
