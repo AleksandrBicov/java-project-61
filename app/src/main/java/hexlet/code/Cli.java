477f382de5b3978a5688b4ae8e452f01b6e28245
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-07 19:19:20 +0300

    change Cli.askName()

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,16 +2,12 @@
 package hexlet.code;
 import java.util.Scanner;
 public final class Cli {
-    public static String name;
-
     public static void askName() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.println("May I have your name?");
-        name = scanner.nextLine();
+        String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
-
-
          scanner.close();
     }
 }
