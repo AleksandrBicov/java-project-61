4c15b31a8ab9300189c7146c8d6012eec594593e
AleksandrBicov <erldimitrovgrad@gmail.com>
2024-03-10 17:42:16 +0300

    change Calc.java

--- app/src/main/java/hexlet/code/games/Calc.java
+++ app/src/main/java/hexlet/code/games/Calc.java
@@ -4,27 +4,26 @@ import hexlet.code.Engine;
 import hexlet.code.Utils;
 
 public final class  Calc {
-    private Calc() {
-    }
     public static void calc() {
         final int finalGenerate = 10;
         String question = "What is the result of the expression?";
         String[][] game = new String[Engine.ROUNDS][2];
+
         for (int index = 0; index < Engine.ROUNDS; index++) {
             int randomValue = Utils.generateNumber(1, finalGenerate);
             int randomValue1 = Utils.generateNumber(1, finalGenerate);
-            int randomOperator = Utils.generateNumber(1, Engine.ROUNDS);
-            char operator = " +-*".charAt(randomOperator);
-            game[index][0] = "" + randomValue + operator + randomValue1;
-            game[index][1] = gameCalc(randomValue, randomValue1, operator);
-        }
-        Engine.engine(question, game);
+            int randomOperator = Utils.generateNumber(0, 2);
+            final char[] operators = {'+', '-', '*'};
 
+            game[index][0] = "" + randomValue + operators[randomOperator] + randomValue1;
+            game[index][1] = String.valueOf(gameCalc(randomValue, randomValue1, operators[randomOperator]));
+        }
+
+        Engine.engine(question, game);
     }
-    public static String gameCalc(final int randomValue, final int randomValue1,
+    public static int gameCalc(final int randomValue, final int randomValue1,
                                   final char operator) {
         int answer = 0;
-        String answerToString;
         switch (operator) {
             case ('+'):
                 answer = randomValue + randomValue1;
@@ -38,7 +37,6 @@ public final class  Calc {
             default:
                 break;
         }
-        answerToString = "" + answer;
-        return answerToString;
+        return answer;
     }
 }
